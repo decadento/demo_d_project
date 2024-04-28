207b6191a30b98bbcf492fc11d77f63852263142
Decadento <sintax3000@gmail.com>
2024-04-28 15:44:01 +0300

    quiz urls fix

--- demo_d/quiz/urls.py
+++ demo_d/quiz/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
   path('get-finish', views.get_finish, name='get-finish'),
 
   path('start_eng', views.start_quiz_view_eng, name='start_eng'),
-  path('get-questions/start_eng', views.get_questions_eng, {'is_start': True}, name='get-questions_eng'),
+  path('get-questions_eng/start', views.get_questions_eng, {'is_start': True}, name='get-questions_eng'),
   path('get-questions_eng', views.get_questions_eng, {'is_start': False}, name='get-questions_eng'),
   path('get-answer_eng', views.get_answer_eng, name='get-answer_eng'),
   path('get-finish_eng', views.get_finish_eng, name='get-finish_eng'),
